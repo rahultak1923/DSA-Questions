c5e59d9b2edf45e09408b675ef4c1593beb9f048
Rahul Tak <rahultak955@gmail.com>
2025-12-18 22:19:11 +0530

    linearsearch and bubblesort

--- questions/searching/linearsearch.js
+++ questions/searching/linearsearch.js
@@ -1,1 +1,29 @@
 // check value one by one 
+let arr = [10, 23, 45, 70, 11, 15];
+let target = 11;
+
+for(let i = 0 ; i<arr.length;i++){
+    if(arr[i]==target){
+        console.log("target index is ", i)
+        break;
+    }
+  
+}
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i; // index
+    }
+  }
+  return -1; // not found
+}
+
+let numbers = [10, 25, 30, 45, 60];
+let result = linearSearch(numbers, 30);
+
+if (result !== -1) {
+  console.log("Element found at index:", result);
+} else {
+  console.log("Element not found");
+}
